42c49db07d6cf9d9ee1c64f0b314c5d8c5476ea5
chris <chris.bhall@outlook.com>
2022-03-20 19:59:10 +0000

    removed localhost call from initial data load

--- src/components/Application.js
+++ src/components/Application.js
@@ -23,9 +23,9 @@ manages application get information/ passing it down as props to daylist & appoi
 
   useEffect(() => {
     Promise.all([
-      axios.get('http://localhost:8001/api/days'),
-      axios.get('http://localhost:8001/api/appointments'),
-      axios.get('http://localhost:8001/api/interviewers')
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
     ]).then((all) => {
       setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewer: all[2].data }));
       const [first, second, third] = all;
